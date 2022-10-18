ed199205dc755b2a058f59f6ca9550e9c2d60ecc
Aso2001005 <83050864+Aso2001005@users.noreply.github.com>
2022-10-18 10:56:16 +0900

    10/18
    controller, entity 追記

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-	
+	@GetMapping("index")
+	public String indexView() {
+		return "index";
+	}
 }
-//aiueo
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,24 @@
 package com.example.demo.entity;
 
-public class User {
+import java.util.Date;
 
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	private Integer user_id;
+	private String user_name;
+	private String icon;
+	private String mail;
+	private String pass;
+	private Integer level;
+	private Date birthday;
+	private Date reg_date;
 }
