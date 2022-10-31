6751e43fef5c8fc4fcfb9ed000bb68229b56fe0d
Aso2001025 <83050417+Aso2001025@users.noreply.github.com>
2022-10-31 10:44:01 +0900

    会員情報変更・画像変更前
    エラーが出た時戻すよう

---
+++ src/main/java/com/example/demo/form/UpdateForm.java
@@ -0,0 +1,13 @@
+package com.example.demo.form;
+
+import java.io.File;
+
+import lombok.Data;
+
+@Data
+public class UpdateForm {
+
+	private File icon;
+	private String mail;
+	private String user_name;
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.entity.User;
 
 public interface UserService {
-	List<User> find(String mail);
+	List<User> findMail(String mail);
+	
+	Optional<User> findId(Integer user_id);
 	
 	Boolean match(List<User> list,String pass);
 	
