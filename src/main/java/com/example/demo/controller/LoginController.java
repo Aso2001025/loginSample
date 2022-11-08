c5b8f1816f5b39022347395685427925fb7ac786
Aso2001025 <83050417+Aso2001025@users.noreply.github.com>
2022-11-08 11:37:41 +0900

    コメント修正

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -29,7 +29,7 @@ import com.example.demo.validator.PassValidator;
 @Controller
 public class LoginController {
 	
-	
+	//Form初期設定エリア
 	@ModelAttribute
 	public UserForm UserSetUpForm() {
 		return new UserForm();
@@ -50,36 +50,45 @@ public class LoginController {
 		return new PassForm();
 	}
 	
+	//sessionnを使えるようにする
 	@Autowired
 	  HttpSession session;
-	
+	//バリデーションを使えるようにする
 	@Autowired
 	PassValidator passValidator;
 	@Autowired
 	MailValidator mailValidator;
+	//サービスを使えるようにする
+	@Autowired
+	private UserService service;
 	
+	//自作バリデーションの導入
 	@InitBinder("inputForm")
 	public void initBinder(WebDataBinder webDataBinder) {
 		webDataBinder.addValidators(passValidator);
 		webDataBinder.addValidators(mailValidator);
 	}
 	
-	@Autowired
-	private UserService service;
 	
 	
+	//ログイン画面への遷移
 	@GetMapping("index")
 	public String indexView() {
 		return "index";
 	}
 	
+	//ログイン処理
 	@PostMapping(value="login",params="com")
 	public String login(@Validated UserForm f,BindingResult bindingResult,Model model) {
+		//バリデーション
 		if(bindingResult.hasErrors()) {
 			return "index";
 		}
+		//メールで検索
 		List<User> list = service.findMail(f.getMail());
+		//メールとパスワードが正しいとき
 		if(!list.isEmpty() && service.match(list.get(0).getPass(), f.getPass())) {
+			//sessionnに値を登録
 			session.setAttribute("user_id", list.get(0).getUser_id());
 			session.setAttribute("user_name", list.get(0).getUser_name());
 			return "login-ok";
@@ -90,81 +99,101 @@ public class LoginController {
 		
 	}
 	
+	//新規登録画面へ遷移
 	@PostMapping(value="login",params="new")
 	public String newInputView() {
 		return "new-input";
 	}
 	
+	//新規登録処理
 	@PostMapping("new")
 	public String newConfirmView(@Validated InputForm f,BindingResult bindingResult,Model model) {
 		if(bindingResult.hasErrors()) {
 			return "new-input";
 		}
+		//パスワードのハッシュ化
 		String hash = service.hash(f.getPass());
+		//現在の日付を取得
 		Date date = service.getDate();
+		//登録用のエンティティの定義
 		User user = new User(null,f.getUser_name(),null,f.getMail(),hash,date);
-
+		//登録処理
 		service.addUser(user);
 		return "new-confirm";
 	}
 	
+	//アカウント詳細へ遷移
 	@PostMapping("acount")
 	public String acountViwe(Model model) {
+		//sessionnに登録されているユーザー情報を取得
 		Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		model.addAttribute("user",user.get());
 		
 		return "acount";
 	}
 	
-	
+	//アイコンの更新画面へ遷移
 	@PostMapping(value="update",params="icon")
 	 public String updateIconView(Model model) {
+		//sessionnに登録されているユーザー情報を取得
 		Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		model.addAttribute(user.get());
 		 return "updateIcon";
 	 }
 	
+	 //メールの更新画面へ遷移
 	 @PostMapping(value="update",params="mail")
 	 public String updateMailView(Model model) {
+		//sessionnに登録されているユーザー情報を取得
 		Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		model.addAttribute(user.get());
 		return "updateMail";
 	 }
 	 
+	 //ユーザーネームの更新画面へ遷移
 	 @PostMapping(value="update",params="user_name")
 	 public String updateNameView(Model model) {
+		//sessionnに登録されているユーザー情報を取得
 		Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		model.addAttribute(user.get());
 		return "updateName";
 	 }
 	 
+	 //パスワードの更新画面へ遷移
 	 @PostMapping(value="update",params="pass")
 	 public String UpdatePassView() {
 		 return "updatePass";
 	 }
+	 //ログアウト処理
 	 @PostMapping(value="update",params="logout")
 	 public String logput(SessionStatus sessionStatus) {
+		 //sessionに登録さてている情報を削除
 		 session.removeAttribute("user_id");
 		 session.removeAttribute("user_name");
 		 session.invalidate();
+		 //sessionのリセット
 		 sessionStatus.setComplete();
 		 return "index";
 	 }
 	 
 	 
-	 
+	 //メール更新処理
 	 @PostMapping("updateMail")
 	 public String updateMail(UpdateForm f,Model model) {
-		 System.out.println(f.getMail());
+		 //更新メソッドへ
 		 service.updateMail(f.getMail(),(Integer)session.getAttribute("user_id"));
+		//sessionnに登録されているユーザー情報を取得
 		 Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		 model.addAttribute("user",user.get());
 		 return "acount";
 	 }
 	 
+	 //ユーザーネーム更新処理
 	 @PostMapping("updateName")
 	 public String updateName(UpdateForm f,Model model) {
+		 //更新メソッドへ
 		 service.updateName(f.getUser_name(),(Integer)session.getAttribute("user_id"));
+		//sessionnに登録されているユーザー情報を取得
 		 Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		 model.addAttribute("user",user.get());
 		 
@@ -172,30 +201,37 @@ public class LoginController {
 	 }
 	 
 	 
+	 //アイコン更新処理
 	 @PostMapping("updateIcon")
 	 public String updateIcon(UpdateForm f,Model model) {
+		 //画像アップロードメソッドへ
 		 String path = service.uploadAction(f.getIcon());
-		 
+		 //更新メソッドへ
 		 service.updateIcon(path,(Integer)session.getAttribute("user_id"));
+		//sessionnに登録されているユーザー情報を取得
 		 Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 		 model.addAttribute("user",user.get());
 		 
 		 return "acount";
 	 }
 	 
+	 //パスワード更新処理
 	 @PostMapping("updatePass")
 	 public String updateIcon(PassForm f,Model model) {
+		 //現在のパスワード取得
 		Optional<User> list = service.findId((Integer) session.getAttribute("user_id"));
+		//入力されたパスワードが正しい場合
 		if(service.match(list.get().getPass(), f.getOldPass())) {
+			//パスワードのハッシュ化
 			String hash = service.hash(f.getNewPass());
-			System.out.println(service.hash(f.getOldPass()));
-			System.out.println(hash);
-			System.out.println(service.hash(f.getNewPass()));
+			//更新画面へ
 			service.updatePass(hash,(Integer)session.getAttribute("user_id"));
+			//sessionnに登録されているユーザー情報を取得
 			Optional<User> user = service.findId((Integer) session.getAttribute("user_id"));
 			model.addAttribute("user",user.get());
 			return "acount";
 		}else {
+			//パスワードが間違ってた場合
 			model.addAttribute("msg","現在のパスワードが違います");
 			return "updatePass";
 		}
